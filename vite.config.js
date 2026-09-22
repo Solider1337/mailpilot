337bf3fcf87d35996dde27cfdf953053264ac8af
Solider1337 <mdyl245@gmail.com>
2026-09-22 19:36:40 +0200

    fix: Add support.html to Vite build inputs

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         taskpane: resolve(__dirname, 'src/taskpane.html'),
         privacy: resolve(__dirname, 'src/privacy.html'),
         terms: resolve(__dirname, 'src/terms.html'),
+        support: resolve(__dirname, 'src/support.html'),
         checkout: resolve(__dirname, 'src/checkout.html'),
         account: resolve(__dirname, 'src/account.html'),
         admin: resolve(__dirname, 'src/admin.html'),
